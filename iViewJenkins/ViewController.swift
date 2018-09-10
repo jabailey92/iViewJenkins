4959578c35cd59c030e58a99c432abebe7a71a30
Jack Bailey <jacknabailey@gmail.com>
2018-09-10 08:11:07 +0100

    Initial Commit

---
+++ iViewJenkins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iViewJenkins
+//
+//  Created by Jack Bailey on 10/09/2018.
+//  Copyright © 2018 Jack Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
